06c09049a874eaaf1aae27ba2b7cbd74f8e7d545
ManoG <manognanaseelan17@gmail.com>
2017-05-24 23:50:53 +0530

    Add files via upload

---
+++ file.php
@@ -0,0 +1,38 @@
+//<?php
+//$pdo = mysqli_connect('host=35.184.184.37','root','GGdk5isPqPgPspyq','booksdb');
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "35.184.184.37";
+$username = "root";
+$password = "GGdk5isPqPgPspyq";
+$dbname = "booksdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM books";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["PID"]. " - Name: ". $row["PNAME"]. "Author: ".$row["AUTHOR"]. "<br>";
+	echp "<br> Published by: ".$row["PUBLISHER"]. "Stock available: ".$row["QUANTITY"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
+
+</body>
+</html>
